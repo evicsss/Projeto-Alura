c6d295f3d917eb7c9a0b277148157538162250c9
Évila Oliveira <evilavictoria@hotmail.com>
2024-09-18 17:59:51 -0300

    add

---
+++ dados.js
@@ -0,0 +1,56 @@
+let dados = [
+    {
+        titulo: "Alagoas",
+        descricao: "Conhecido por suas belezas naturais, rica cultura e importância econômica crescente. Com a capital em Maceió, o estado oferece uma combinação fascinante de praias paradisíacas, história colonial e tradições culturais. Alagoas é famoso por seu litoral deslumbrante, que inclui praias de águas cristalinas e coqueirais, como as de Pajuçara, Ponta Verde e São Miguel dos Milagres. O estado também possui áreas de lagoas e dunas, destacando-se a Lagoa do Mundaú e a Lagoa do Manguaba, que são importantes para o ecossistema local. O clima predominante é tropical, com uma estação chuvosa de março a agosto e uma estação seca no restante do ano.",
+        link: "https://pt.wikipedia.org/wiki/Alagoas",
+        tags: "maceio al "
+      },
+      {
+        titulo: "Bahia",
+        descricao: "A Bahia tem uma rica história que remonta ao período colonial. Salvador, fundada em 1549, foi a primeira capital do Brasil e desempenhou um papel central na colonização portuguesa. A cidade é um importante centro cultural e histórico, com um centro histórico que é Patrimônio Mundial da UNESCO. A Bahia também é conhecida por sua influência na luta pela independência do Brasil e sua rica contribuição para a cultura afro-brasileira. A cultura baiana é uma das mais vibrantes do Brasil, refletindo uma rica mistura de influências africanas, indígenas e portuguesas. A música é um dos aspectos mais destacados, com gêneros como axé, samba, e música popular brasileira (MPB) sendo amplamente apreciados. O Carnaval de Salvador é um dos maiores e mais famosos do mundo, atraindo milhões de foliões a cada ano para as ruas da cidade. Além disso, a Bahia é o berço de manifestações culturais como o candomblé e a capoeira.",
+        link: "https://pt.wikipedia.org/wiki/Bahia",
+        tags: "salvador pelourinho itacare axe" 
+      },
+      {
+        titulo: "Ceará",
+        descricao: "A capital do estado é Fortaleza, uma das maiores cidades do Nordeste e um importante centro econômico e turístico. Possui uma variedade de paisagens, que vão desde o litoral com praias paradisíacas até o sertão e áreas de caatinga no interior. O estado é famoso por suas dunas, lagoas e falésias ao longo da costa. O clima é predominantemente tropical, com uma estação chuvosa que vai de fevereiro a maio e uma estação seca que pode se estender até o final do ano, especialmente no sertão.",
+        link: "https://pt.wikipedia.org/wiki/Ceara",
+        tags: "Jericoacoara fortaleza ceara "
+      },
+      {
+        titulo: "Maranhão",
+        descricao: "A capital do estado é São Luís, uma cidade conhecida por seu patrimônio histórico e arquitetura colonial. O estado é conhecido por sua música tradicional, como o bumba meu boi, uma festa folclórica que combina dança, música e teatro. Outras manifestações culturais incluem o tambor de crioula e o cacuriá, ambos refletem a rica herança afro-brasileira.",
+        link: "https://pt.wikipedia.org/wiki/Maranhao",
+        tags: "sao luis lençóis maranhenses maranhao"
+      },
+      {
+        titulo: "Paraíba",
+        descricao: "A capital do estado é João Pessoa, que é uma das cidades mais antigas do Brasil e conhecida por suas praias e patrimônio histórico. A Paraíba apresenta uma diversidade de paisagens, desde o litoral com praias como Tambaú e Cabo Branco até o sertão no interior, com um clima predominantemente semiárido. O estado possui áreas de caatinga e também zonas de mata atlântica, especialmente na região do Brejo. ",
+        link: "https://pt.wikipedia.org/wiki/Paraiba",
+        tags: "joão pessoa praia da tabatinga campina grande Transamazônica paraiba pb"
+      },
+      {
+        titulo: "Pernambuco",
+        descricao: "Conhecido por sua rica diversidade cultural, histórica e geográfica. Sua capital é Recife, que também é um dos maiores centros urbanos e econômicos da região. Geograficamente, o estado possui uma variedade de paisagens, incluindo praias, zonas de mata atlântica e áreas de caatinga. O litoral pernambucano é famoso por suas praias paradisíacas, como Porto de Galinhas e Praia dos Carneiros, que atraem turistas de todo o Brasil e do mundo. Também é marcada pelo frevo e pelo maracatu.",
+        link: "https://pt.wikipedia.org/wiki/Pernambuco",
+        tags: "fernando de noronha recife olinda caruaru maracatu frevo pe"
+      },
+      {
+        titulo: "Piauí",
+        descricao: "Estado com paisagens diversificadas, desde o litoral até o sertão, com território que se divide entre as sub-regiões do Meio Norte e do Sertão. Sua capital é o município de Teresina, o município mais populoso do estado. No total, o Piauí possui 3.271.199 habitantes. A pecuária consiste em uma importante atividade econômica desenvolvida no estado.",
+        link: "https://pt.wikipedia.org/wiki/Piaui",
+        tags: "piaui teresina piauiense Parque Nacional da Serra da Capivara Parnaíba piaui pi"
+      },
+      {
+        titulo: "Rio Grande do Norte",
+        descricao: "Sua capital é o município de Natal, no litoral leste. Pertence ao território potiguar o Atol das Rocas, formação única no Atlântico Sul e patrimônio natural mundial pela Unesco. O relevo do estado é formado em sua maioria por depressões, e o clima predominante é o Tropical, com variações regionais. Em termos econômicos, o Rio Grande do Norte desponta como principal produtor de camarão e de sal marinho do Brasil.",
+        link: "https://pt.wikipedia.org/wiki/Rio_Grande_do_Norte",
+        tags: "natal pipa mossoro rs RS  "
+      },
+      {
+        titulo: "Sergipe",
+        descricao: "Consiste na menor unidade da federação em extensão territorial. A capital sergipana é Aracaju, seu município mais populoso. No total, o estado conta com pouco mais de 2,3 milhões de habitantes. Os tipos climáticos predominantes são o semiárido e o tropical úmido, respectivamente no oeste e no leste do território sergipano. O suco de laranja é o carro-chefe das exportações do estado, enquanto a cana-de-açúcar lidera a produção agrícola. Na indústria, o destaque fica com a construção civil.",
+        link: "https://pt.wikipedia.org/wiki/Sergipe",
+        tags: "sergipe se aracaju rio sao francisco lambe sujo caboclinhos atalaia sao cristovao "
+      }
+]
